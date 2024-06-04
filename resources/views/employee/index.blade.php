67be35eddb57f8dc2ee7bb799e9ccdcffd9a9ab3
realkeiji <johnbanag42@gmail.com>
2024-06-04 13:56:24 +0800

    eto pre

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -2,212 +2,26 @@
 
   @section('content')
 
-      <div class="container mt-5">
+  <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>BSIT-3A</h3>
 
-          <h3 align="center" class="mt-5">EMPLOYEE TABLE</h3>
-
-          <div class="row">
-              
-              <div class= "card-body">
-                  <table class= "table table-bordered table-striped">
-                      <thead>
-                          <tr>
-                              <th>ID</th>
-                              <th>First Name</th>
-                              <th>Middle Name</th>
-                              <th>Last Name</th>
-                              <th>Address</th>
-                              <th>Country</th>
-                              <th>Zip</th>
-                              <th>Age</th>
-                              <th>BirthDate</th>
-                              <th>Date Hired</th>
-                              <th>Department</th>
-                              <th>Division</th>
-                          </tr> 
-                      </thead>
-                      <tbody>
-                          @foreach ($employment as $identify)
-                          <tr>
-                              <td>{{$identify->id}}</td>
-                              <td>{{$identify->firstname}}</td>
-                              <td>{{$identify->middlename}}</td>
-                              <td>{{$identify->lastname}}</td>
-                              <td>{{$identify->address}}</td>
-                              <td>{{$identify->country}}</td>
-                              <td>{{$identify->zip}}</td>
-                              <td>{{$identify->age}}</td>
-                              <td>{{$identify->birth}}</td>
-                              <td>{{$identify->DOB}}</td>
-                              <td>{{$identify->dept}}</td>
-                              <td>{{$identify->division}}</td>
-                              
-                          </tr>
-                          @endforeach
-                      </tbody>
-                  </table>
-                  <a href="{{url('employee/create')}}" class="btn btn-primary">ADD</a>
+                <p>EXAMINERS</p>
               </div>
-          </div>
-      </div>
-
-      <div class="row">
-            <div class="col-md-12">
-              <div class="card">
-                <div class="card-header">
-                  <h5 class="card-title">Monthly Recap Report</h5>
-
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-minus"></i>
-                    </button>
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                        <i class="fas fa-wrench"></i>
-                      </button>
-                      <div class="dropdown-menu dropdown-menu-right" role="menu">
-                        <a href="#" class="dropdown-item">Action</a>
-                        <a href="#" class="dropdown-item">Another action</a>
-                        <a href="#" class="dropdown-item">Something else here</a>
-                        <a class="dropdown-divider"></a>
-                        <a href="#" class="dropdown-item">Separated link</a>
-                      </div>
-                    </div>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove">
-                      <i class="fas fa-times"></i>
-                    </button>
-                  </div>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-8">
-                      <p class="text-center">
-                        <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
-                      </p>
-
-                      <div class="chart">
-                        <!-- Sales Chart Canvas -->
-                        <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
-                      </div>
-                      <!-- /.chart-responsive -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-md-4">
-                      <p class="text-center">
-                        <strong>Goal Completion</strong>
-                      </p>
-
-                      <div class="progress-group">
-                        Add Products to Cart
-                        <span class="float-right"><b>160</b>/200</span>
-                        <div class="progress progress-sm">
-                          <div class="progress-bar bg-primary" style="width: 80%"></div>
-                        </div>
-                      </div>
-                      <!-- /.progress-group -->
-
-                      <div class="progress-group">
-                        Complete Purchase
-                        <span class="float-right"><b>310</b>/400</span>
-                        <div class="progress progress-sm">
-                          <div class="progress-bar bg-danger" style="width: 75%"></div>
-                        </div>
-                      </div>
-
-                      <!-- /.progress-group -->
-                      <div class="progress-group">
-                        <span class="progress-text">Visit Premium Page</span>
-                        <span class="float-right"><b>480</b>/800</span>
-                        <div class="progress progress-sm">
-                          <div class="progress-bar bg-success" style="width: 60%"></div>
-                        </div>
-                      </div>
-
-                      <!-- /.progress-group -->
-                      <div class="progress-group">
-                        Send Inquiries
-                        <span class="float-right"><b>250</b>/500</span>
-                        <div class="progress progress-sm">
-                          <div class="progress-bar bg-warning" style="width: 50%"></div>
-                        </div>
-                      </div>
-                      <!-- /.progress-group -->
-                    </div>
-                    <!-- /.col -->
-                  </div>
-                  <!-- /.row -->
-                </div>
-                <!-- ./card-body -->
-                <div class="card-footer">
-                  <div class="row">
-                    <div class="col-sm-3 col-6">
-                      <div class="description-block border-right">
-                        <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
-                        <h5 class="description-header">$35,210.43</h5>
-                        <span class="description-text">TOTAL REVENUE</span>
-                      </div>
-                      <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-3 col-6">
-                      <div class="description-block border-right">
-                        <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
-                        <h5 class="description-header">$10,390.90</h5>
-                        <span class="description-text">TOTAL COST</span>
-                      </div>
-                      <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-3 col-6">
-                      <div class="description-block border-right">
-                        <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 20%</span>
-                        <h5 class="description-header">$24,813.53</h5>
-                        <span class="description-text">TOTAL PROFIT</span>
-                      </div>
-                      <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-3 col-6">
-                      <div class="description-block">
-                        <span class="description-percentage text-danger"><i class="fas fa-caret-down"></i> 18%</span>
-                        <h5 class="description-header">1200</h5>
-                        <span class="description-text">GOAL COMPLETIONS</span>
-                      </div>
-                      <!-- /.description-block -->
-                    </div>
-                  </div>
-                  <!-- /.row -->
-                </div>
-                <!-- /.card-footer -->
+              <div class="icon">
+                <i class="ion ion-bag"></i>
               </div>
-              <!-- /.card -->
+              <a href="{{url('employee/dashboard')}}" class="small-box-footer">Redirect to Table <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <!-- /.col -->
           </div>
-          <!-- /.row -->
+
+    
 
   @endsection
 
-
-  @push('css')
-      <style>
-          .form-area{
-              padding: 20px;
-              margin-top: 20px;
-                background-color:#FFFF00;
-          }
-
-          .bi-trash-fill{
-              color:red;
-              font-size: 18px;
-          }
-
-          .bi-pencil{
-              color:green;
-              font-size: 18px;
-              margin-left: 20px;
-          }
-      </style>
-
-  @endpush
